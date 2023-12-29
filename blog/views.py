5279ce2bfe49312e69d3925ff12eca8a43ccea43
aaron-norman <for.aaronnorman@gmail.com>
2023-12-29 17:38:14 -0500

    created initial views / url patterns.

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+    return HttpResponse(
+        "<h1> Blog Home </h1>"
+        "<h2> Welcome Home !</h2>"
+)
+
+def about(request):
+    return HttpResponse("<h1>About page</h1>")
